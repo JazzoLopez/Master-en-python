d4ac62b38b2a22ae8f8e6156a467f10cf67f97ce
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-11-17 15:59:57 -0600

    Seccion 3

---
+++ curiosidades.py
@@ -0,0 +1,6 @@
+mi_texto = "hola"
+#mi-texto2 = "numdo" el caracter "-" no es valido
+miTexto2 = "Mundo"
+
+texto = mi_texto + " " + miTexto2
+print(texto) 
--- dataTypes.py
+++ dataTypes.py
@@ -1,7 +1,41 @@
 # NONE, no tiene nada la variable
-nada = None
-
-print (nada)
+nothing = None
+string = "Esta es una cadena de texto, str esaa de string lmao"
+int = 99
+float = 3.4
+#A FUERZA LA PRIMERA ES CON LETRA ,MAYUSCULA EN LOS BOOLEANS
+boolean = True
+#DIGAMOS UN ARREGLO
+list = [1, "pato", 3, 4, False]
+tuple = ("master",2,"Jazziel")
+#COMO UN JSON, ES DE CLAVE Y VALOR
+dict = {
+    "name": "Jazziel",
+    "lastname": "Rodriguez",
+    "curso": 1
+}
+range = range(9)
+byte = b"prueba"
+#IMPRIMENDO EL CONTENIDO
+print (nothing)
+print(tuple)
+print(dict)
+print(range)
+print(byte)
 
 # Mostrar tipo de dato
-print(type(nada))
+print(type(nothing))
+print(type(string))
+print(type(int))
+print(type(float))
+print(type(boolean))
+print(type(list))
+print(type(tuple))
+print(type(dict))
+print(type(range))
+print(type(byte))
+
+texto = "Hola soy un texto"
+numero = str(776)
+print(texto + " " + numero)
+print(type(numero))
