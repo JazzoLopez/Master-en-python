22a9fee38ba4871143f36725a2a80c421c54fac8
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-11-17 16:03:42 -0600

    Actualizando

--- curiosidades.py
+++ curiosidades.py
@@ -1,6 +1,9 @@
 mi_texto = "hola"
 #mi-texto2 = "numdo" el caracter "-" no es valido
-miTexto2 = "Mundo"
+miTexto2 = "Mundo soy 'Jazziel' y estoy aprendiendo \"python\""
 
-texto = mi_texto + " " + miTexto2
+#\t es una tabulacion 
+#\r borra todo lo que este antes de este
+#\n es un salto de linea, el basico 
+texto = mi_texto + " \n" + miTexto2
 print(texto) 
